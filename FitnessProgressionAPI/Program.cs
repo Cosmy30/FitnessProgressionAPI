cfcf47c7c0084a180cb20149a32dc834fb37b460
Cosmin <cosminbalaiu@yahoo.com>
2026-03-27 17:42:55 +0200

    feat: Add Swagger configuration in Program.cs

--- FitnessProgressionAPI/Program.cs
+++ FitnessProgressionAPI/Program.cs
@@ -4,10 +4,25 @@ using FitnessProgressionAPI.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(options => 
+        options.SwaggerDoc("v1", new()
+        {
+            Title = "FitnessProgressionAPI",
+            Version = "v1",
+            Description = "API for tracking workout progress"
+        }));
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.MapControllers();
 
