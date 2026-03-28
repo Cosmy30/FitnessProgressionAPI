74b4d224d0e55c08c540eb3998e8373ffde6c6a1
Cosmin <cosminbalaiu@yahoo.com>
2026-03-28 18:58:39 +0200

    feat: Add UserResponseDto for GET endpoints

---
+++ FitnessProgressionAPI/DTOs/UserResponseDto.cs
@@ -0,0 +1,12 @@
+﻿namespace FitnessProgressionAPI.DTOs
+{
+    public class UserResponseDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public decimal Weight { get; set; }
+
+    }
+}
