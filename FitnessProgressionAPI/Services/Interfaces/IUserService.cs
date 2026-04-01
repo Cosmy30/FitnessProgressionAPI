46f49a33b699f7760e6ed7902b899b3935afe034
Cosmin <cosminbalaiu@yahoo.com>
2026-04-01 18:04:08 +0300

    feat(users): add IUserService for service abstraction

---
+++ FitnessProgressionAPI/Services/Interfaces/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using FitnessProgressionAPI.Models;
+
+namespace FitnessProgressionAPI.Services.Interfaces
+{
+    public interface IUserService
+    {
+        public Task<List<User>> GetAll();
+        public Task<User> GetById(int id);
+        public Task<User> Create();
+        public Task<User> UpdatePartial(int id);
+        public Task<bool> Delete(int id);
+
+    }
+}
