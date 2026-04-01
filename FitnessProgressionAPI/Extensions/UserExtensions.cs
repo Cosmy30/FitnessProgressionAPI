75b2162d68bff1aac7d319306778e7b6d6af87b3
Cosmin <cosminbalaiu@yahoo.com>
2026-04-01 19:10:58 +0300

    feat(users): implement UserExtensions for DTO mapping

---
+++ FitnessProgressionAPI/Extensions/UserExtensions.cs
@@ -0,0 +1,20 @@
+﻿using FitnessProgressionAPI.DTOs;
+using FitnessProgressionAPI.Models;
+
+namespace FitnessProgressionAPI.Extensions
+{
+    public static class UserExtensions
+    {
+        public static UserResponseDto ToDto(this User user)
+        {
+            return new UserResponseDto
+            {
+                Id = user.Id,
+                Name = user.Name,
+                Email = user.Email,
+                DateOfBirth = user.DateOfBirth,
+                Weight = user.Weight
+            };
+        }
+    }
+}
