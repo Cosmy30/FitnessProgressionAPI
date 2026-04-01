cbb907e4a5115cdbc559e97120da4e12aea2e5c1
Cosmin <cosminbalaiu@yahoo.com>
2026-04-01 18:13:30 +0300

    feat(users): add UserService for business logic

---
+++ FitnessProgressionAPI/Services/Implementations/UserService.cs
@@ -0,0 +1,41 @@
+﻿using FitnessProgressionAPI.Services.Interfaces;
+using FitnessProgressionAPI.Data;
+using FitnessProgressionAPI.Models;
+
+namespace FitnessProgressionAPI.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        private readonly AppDbContext _context;
+
+        public UserService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<User>> GetAll()
+        {
+
+        }
+
+        public async Task<User> GetById(int id)
+        {
+            
+        }
+
+        public async Task<User> Create()
+        {
+
+        }
+
+        public async Task<User> UpdatePartial(int id)
+        {
+
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+
+        }
+    }
+}
