da7ecaa765e34957022e947673b89f4ed6935c9c
Cosmin <cosminbalaiu@yahoo.com>
2026-04-02 18:02:20 +0300

    refactor(users): use expression tree for DTO mapping to enable EF translation

--- FitnessProgressionAPI/Extensions/UserExtensions.cs
+++ FitnessProgressionAPI/Extensions/UserExtensions.cs
@@ -1,19 +1,20 @@
 ﻿using FitnessProgressionAPI.DTOs;
 using FitnessProgressionAPI.Models;
+using System.Linq.Expressions;
 
 namespace FitnessProgressionAPI.Extensions
 {
     public static class UserExtensions
     {
-        public static UserResponseDto ToDto(this User user)
+        public static Expression<Func<User, UserResponseDto>> ToDto()
         {
-            return new UserResponseDto
+            return u => new UserResponseDto
             {
-                Id = user.Id,
-                Name = user.Name,
-                Email = user.Email,
-                DateOfBirth = user.DateOfBirth,
-                Weight = user.Weight
+                Id = u.Id,
+                Name = u.Name,
+                Email = u.Email,
+                DateOfBirth = u.DateOfBirth,
+                Weight = u.Weight
             };
         }
     }
