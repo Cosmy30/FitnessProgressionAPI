f4093366108a093d7745fc6fd7bae709be27c544
Cosmin <cosminbalaiu@yahoo.com>
2026-04-06 16:24:01 +0300

    refactor(users): move mappings methods from UserExtensions to UserMappings

--- FitnessProgressionAPI/Extensions/UserExtensions.cs
+++ FitnessProgressionAPI/Extensions/UserExtensions.cs
@@ -1,21 +1,10 @@
 ﻿using FitnessProgressionAPI.DTOs;
 using FitnessProgressionAPI.Models;
-using System.Linq.Expressions;
 
 namespace FitnessProgressionAPI.Extensions
 {
     public static class UserExtensions
     {
-        public static Expression<Func<User, UserResponseDto>> ToDto()
-        {
-            return u => new UserResponseDto
-            {
-                Id = u.Id,
-                Name = u.Name,
-                Email = u.Email,
-                DateOfBirth = u.DateOfBirth,
-                Weight = u.Weight
-            };
-        }
+
     }
 }
---
+++ FitnessProgressionAPI/Mappings/UserMappings.cs
@@ -0,0 +1,45 @@
+﻿using FitnessProgressionAPI.DTOs;
+using FitnessProgressionAPI.Models;
+using System.Linq.Expressions;
+
+namespace FitnessProgressionAPI.Mappings
+{
+    public static class UserMappings
+    {
+        public static Expression<Func<User, UserResponseDto>> ToDtoExpression()
+        {
+            return u => new UserResponseDto
+            {
+                Id = u.Id,
+                Name = u.Name,
+                Email = u.Email,
+                DateOfBirth = u.DateOfBirth,
+                Weight = u.Weight
+            };
+        }
+
+        public static UserResponseDto ToDto(this User user)
+        {
+            return new UserResponseDto
+            {
+                Id = user.Id,
+                Name = user.Name,
+                Email = user.Email,
+                DateOfBirth = user.DateOfBirth,
+                Weight = user.Weight
+            };
+        }
+
+        public static User ToUser(this CreateUserDto dto)
+        {
+            return new User
+            {
+                Name = dto.Name,
+                Email = dto.Email,
+                PasswordHash = BCrypt.Net.BCrypt.HashPassword(dto.Password),
+                DateOfBirth = dto.DateOfBirth,
+                Weight = dto.Weight
+            };
+        }
+    }
+}
