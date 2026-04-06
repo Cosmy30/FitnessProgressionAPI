dfdae8e4dea523b0a6f04786a914e176b1239803
Cosmin <cosminbalaiu@yahoo.com>
2026-04-06 18:28:47 +0300

    refactor(users): extract patch update logic to extension method

--- FitnessProgressionAPI/Extensions/UserExtensions.cs
+++ FitnessProgressionAPI/Extensions/UserExtensions.cs
@@ -5,6 +5,12 @@ namespace FitnessProgressionAPI.Extensions
 {
     public static class UserExtensions
     {
-
+        public static void ApplyUpdate(this User user, UpdateUserDto dto)
+        {
+            user.Name = dto.Name ?? user.Name;
+            user.Email = dto.Email ?? user.Email;
+            user.DateOfBirth = dto.DateOfBirth ?? user.DateOfBirth;
+            user.Weight = dto.Weight ?? user.Weight;
+        }
     }
 }
