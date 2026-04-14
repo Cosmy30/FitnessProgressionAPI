7edbec7a20274c2e8070ef14c950954588f71a33
Cosmin <cosminbalaiu@yahoo.com>
2026-04-14 13:35:01 +0300

    feat(workouts): add IWorkoutService

---
+++ FitnessProgressionAPI/Services/Interfaces/IWorkoutService.cs
@@ -0,0 +1,12 @@
+﻿using FitnessProgressionAPI.DTOs.Workouts;
+
+namespace FitnessProgressionAPI.Services.Interfaces
+{
+    public interface IWorkoutService
+    {
+        public Task<WorkoutResponseDto?> GetById(int id);
+        public Task<WorkoutResponseDto> Create(CreateWorkoutDto dto);
+        public Task<WorkoutResponseDto?> Patch(int id, UpdateWorkoutDto dto);
+        public Task<bool> DeleteById(int id);
+    }
+}
