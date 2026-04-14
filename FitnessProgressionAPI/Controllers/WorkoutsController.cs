6c4983571a136b5181cb62b7d0f695e1b1924a5b
Cosmin <cosminbalaiu@yahoo.com>
2026-04-14 13:45:00 +0300

    feat(workouts): add workouts controller

---
+++ FitnessProgressionAPI/Controllers/WorkoutsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FitnessProgressionAPI.Services.Interfaces;
+
+namespace FitnessProgressionAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WorkoutsController : ControllerBase
+    {
+        private readonly IWorkoutService _workoutService;
+        
+        public WorkoutsController(IWorkoutService workoutService)
+        {
+            _workoutService = workoutService;
+        }
+    }
+}
