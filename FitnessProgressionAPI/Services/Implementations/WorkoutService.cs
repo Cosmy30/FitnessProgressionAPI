23ec2483fcd59a6d46046e159895cbdb56e7f3bc
Cosmin <cosminbalaiu@yahoo.com>
2026-04-14 13:47:15 +0300

    feat(workouts): add workouts service

---
+++ FitnessProgressionAPI/Services/Implementations/WorkoutService.cs
@@ -0,0 +1,15 @@
+﻿using FitnessProgressionAPI.Data;
+using FitnessProgressionAPI.Services.Interfaces;
+
+namespace FitnessProgressionAPI.Services.Implementations
+{
+    public class WorkoutService : IWorkoutService
+    {
+        private readonly AppDbContext _context;
+
+        public WorkoutService(AppDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
