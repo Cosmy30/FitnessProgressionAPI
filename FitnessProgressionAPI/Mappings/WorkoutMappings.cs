4441fd4ec7c2354541f84cd670edc9f85b9ac3e9
Cosmin <cosminbalaiu@yahoo.com>
2026-04-14 13:29:06 +0300

    feat(workouts): add ToDtoExpression for projection

---
+++ FitnessProgressionAPI/Mappings/WorkoutMappings.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq.Expressions;
+using FitnessProgressionAPI.Models;
+using FitnessProgressionAPI.DTOs.Workouts;
+
+namespace FitnessProgressionAPI.Mappings
+{
+    public static class WorkoutMappings
+    {
+        public static Expression<Func<Workout, WorkoutResponseDto>> ToDtoExpression()
+        {
+            return w => new WorkoutResponseDto
+            {
+                Id = w.Id,
+                WorkoutDate = w.WorkoutDate,
+                DurationMinutes = w.DurationMinutes,
+                Notes = w.Notes,
+                Type = w.Type
+            };
+        }
+    }
+}
