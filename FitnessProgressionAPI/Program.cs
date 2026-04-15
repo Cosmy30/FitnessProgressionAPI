43d6ac4d52646176b92a9f88827521d047a5dad4
Cosmin <cosminbalaiu@yahoo.com>
2026-04-15 14:19:11 +0300

    feat(workouts): add IWorkoutService to DI container

--- FitnessProgressionAPI/Program.cs
+++ FitnessProgressionAPI/Program.cs
@@ -19,6 +19,7 @@ builder.Services.AddSwaggerGen(options =>
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IWorkoutService, WorkoutService>();
 
 var app = builder.Build();
 
