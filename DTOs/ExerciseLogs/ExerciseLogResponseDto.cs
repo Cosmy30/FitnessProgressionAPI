c7dc2be04267c47596ff9b1d3f9b211eb8df9109
Cosmin <cosminbalaiu@yahoo.com>
2026-04-23 18:02:04 +0300

    feat(exercise-logs): add ExerciseLogResponseDto

---
+++ DTOs/ExerciseLogs/ExerciseLogResponseDto.cs
@@ -0,0 +1,19 @@
+﻿namespace FitnessProgressionAPI.DTOs.ExerciseLogs
+{
+    public class ExerciseLogResponseDto
+    {
+        public int Id { get; set; }
+
+        public int ExerciseId { get; set; }
+
+        public string ExerciseName { get; set; } = null!;
+
+        public int Sets { get; set; }
+
+        public int Reps { get; set; }
+
+        public decimal Weight { get; set; }
+
+        public int RestSeconds { get; set; }
+    }
+}
