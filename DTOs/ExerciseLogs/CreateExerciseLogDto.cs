6283818a6839fde42bccbcd955f286693e9c14a2
Cosmin <cosminbalaiu@yahoo.com>
2026-04-23 15:28:51 +0300

    feat(exercise-logs): add CreateExerciseLogDto

---
+++ DTOs/ExerciseLogs/CreateExerciseLogDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FitnessProgressionAPI.DTOs.ExerciseLogs
+{
+    public class CreateExerciseLogDto
+    {
+        [Range(1, int.MaxValue)]
+        public int ExerciseId { get; set; }
+
+        [Range(1, 100)]
+        public int? Sets { get; set; }
+
+        [Range(1, 1000)]
+        public int? Reps { get; set; }
+
+        [Range(1, 1000)]
+        public decimal? Weight { get; set; }
+
+        [Range(1, 3600)]
+        public int? RestSeconds { get; set; }
+    }
+}
