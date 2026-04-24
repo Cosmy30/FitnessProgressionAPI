22910ba147b703873338fcfdf03d5edf61949bf2
Cosmin <cosminbalaiu@yahoo.com>
2026-04-24 16:15:23 +0300

    feat(exercises): add migration for Name property

---
+++ Migrations/20260424125410_UpdateExerciseSchema.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FitnessProgressionAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateExerciseSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Exercises",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Exercises");
+        }
+    }
+}
