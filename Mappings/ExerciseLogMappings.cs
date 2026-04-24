8dc4ec1aaee0455a166449fefe970b9c106fc696
Cosmin <cosminbalaiu@yahoo.com>
2026-04-24 16:37:32 +0300

    refactor(exercise-logs): add projection for ExerciseLogResponseDto

--- Mappings/ExerciseLogMappings.cs
+++ Mappings/ExerciseLogMappings.cs
@@ -1,7 +1,23 @@
-﻿namespace FitnessProgressionAPI.Mappings
+﻿using FitnessProgressionAPI.DTOs.ExerciseLogs;
+using FitnessProgressionAPI.Models;
+using System.Linq.Expressions;
+
+namespace FitnessProgressionAPI.Mappings
 {
     public static class ExerciseLogMappings
     {
-        
+        public static Expression<Func<ExerciseLog, ExerciseLogResponseDto>> ToDtoExpression()
+        {
+            return u => new ExerciseLogResponseDto
+            {
+                Id = u.Id,
+                ExerciseId = u.ExerciseId,
+                ExerciseName = u.Exercise.Name,
+                Sets = u.Sets,
+                Reps = u.Reps,
+                Weight = u.Weight,
+                RestSeconds = u.RestSeconds
+            };
+        }
     }
 }
