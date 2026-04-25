a97c9153b4ffa88e37e5a9351138a4ce85f51c70
Cosmin <cosminbalaiu@yahoo.com>
2026-04-25 15:05:24 +0300

    refactor(exercise-logs): add patch update helper for ExerciseLog

---
+++ Extensions/ExerciseLogExtensions.cs
@@ -0,0 +1,16 @@
+﻿using FitnessProgressionAPI.Models;
+using FitnessProgressionAPI.DTOs.ExerciseLogs;
+
+namespace FitnessProgressionAPI.Extensions
+{
+    public static class ExerciseLogExtensions
+    {
+        public static void ApplyUpdate(this ExerciseLog exerciseLog, UpdateExerciseLogDto dto)
+        {
+            exerciseLog.Sets = dto.Sets ?? exerciseLog.Sets;
+            exerciseLog.Reps = dto.Reps ?? exerciseLog.Reps;
+            exerciseLog.Weight = dto.Weight ?? exerciseLog.Weight;
+            exerciseLog.RestSeconds = dto.RestSeconds ?? exerciseLog.RestSeconds;
+        }
+    }
+}
