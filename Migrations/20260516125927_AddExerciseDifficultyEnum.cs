05bb7e773dedcb7f18e6ae03158cbf91f6f5780f
Cosmin <cosminbalaiu@yahoo.com>
2026-05-16 16:15:50 +0300

    feat(exercises): add migration for DifficultyLevel enum

---
+++ Migrations/20260516125927_AddExerciseDifficultyEnum.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FitnessProgressionAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddExerciseDifficultyEnum : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
