bd81174bcac7209d829bebc946b8464e30111ae9
Cosmin <cosminbalaiu@yahoo.com>
2026-05-17 15:35:36 +0300

    feat(exercises): add migration for ExerciseCategoryEnum

---
+++ Migrations/20260517122506_AddExerciseCategoryEnum.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FitnessProgressionAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddExerciseCategoryEnum : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Category",
+                table: "Exercises",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Category",
+                table: "Exercises",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
