6a092e891c0f3544a3662b1e3ac4642ff6f0ef7e
Cosmin <cosminbalaiu@yahoo.com>
2026-05-18 16:57:54 +0300

    chore(exercises): add scaffolded service, interface and controller

---
+++ Controllers/ExercisesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FitnessProgressionAPI.Services.Interfaces;
+
+namespace FitnessProgressionAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ExercisesController : ControllerBase
+    {
+        private readonly IExerciseService _exerciseService;
+
+        public ExercisesController(IExerciseService exerciseService)
+        {
+            _exerciseService = exerciseService;
+        }
+    }
+}
