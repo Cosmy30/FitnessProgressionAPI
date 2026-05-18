dbebb2d08d681a8abbaebaff78f01ccdc7668ba5
Cosmin <cosminbalaiu@yahoo.com>
2026-05-18 16:16:28 +0300

    feat(exercises): add method for projection from Exercise to DTO

---
+++ Mappings/ExerciseMappings.cs
@@ -0,0 +1,21 @@
+﻿using FitnessProgressionAPI.DTOs.Exercises;
+using FitnessProgressionAPI.Models;
+using System.Linq.Expressions;
+
+namespace FitnessProgressionAPI.Mappings
+{
+    public static class ExerciseMappings
+    {
+        public static Expression<Func<Exercise, ExerciseResponseDto>> ToDtoExpression()
+        {
+            return e => new ExerciseResponseDto
+            {
+                Id = e.Id,
+                Name = e.Name,
+                Category = e.Category,
+                DifficultyLevel = e.DifficultyLevel,
+                Family = e.Family
+            };
+        }
+    }
+}
