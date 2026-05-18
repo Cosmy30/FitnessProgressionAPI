492e64ac5c9b6a303d882a518fd9b5fa886091f4
Cosmin <cosminbalaiu@yahoo.com>
2026-05-18 16:02:11 +0300

    feat(exercises): add ExerciseResponseDto

---
+++ DTOs/Exercises/ExerciseResponseDto.cs
@@ -0,0 +1,17 @@
+﻿using FitnessProgressionAPI.Enums;
+
+namespace FitnessProgressionAPI.DTOs.Exercises
+{
+    public class ExerciseResponseDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public ExerciseCategory Category { get; set; }
+
+        public DifficultyLevel DifficultyLevel { get; set; }
+
+        public string Family { get; set; } = null!;
+    }
+}
